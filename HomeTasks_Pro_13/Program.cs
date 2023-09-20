560e26d11cf08d4834613494937b22c3fbcab790
Shikizuma <myroslav.bashkirov@gmail.com>
2023-09-20 19:01:54 +0300

    Add project files.

---
+++ HomeTasks_Pro_13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+
+namespace HomeTasks_Pro_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = Task.Run(Method1);
+            Task task2 = Task.Run(Method2);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('*');
+                Thread.Sleep(100);
+                Console.WriteLine("\nОсновний потік продовжує виконання.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Основний потік завершив виконання.");
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("\nMethod1 почав виконання.");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('|');
+                Thread.Sleep(50);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Method1 завершив виконання.");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("\nMethod2 почав виконання.");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write('_');
+                Thread.Sleep(50);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Method2 завершив виконання.");
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 1_000_000;
+            Random random = new Random();
+            int[] array = new int[size];
+            Parallel.For(0, size, (i) => array[i] = random.Next(1, 10));
+
+            ParallelQuery<int> odds = from elem in array.AsParallel()
+                                      where elem % 2 != 0
+                                      select elem;
+
+            foreach (int element in odds)
+                Console.Write(element + " ");
+        }
+    }
+}
